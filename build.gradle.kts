b5072cbb0a206a6689501cc9c046ed31f4261f8d
刘平 <aa@gmail.com>
2025-04-08 08:22:22 +0800

    temp

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,22 +19,18 @@ repositories {
     mavenCentral()
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-//intellij {
-//    version.set("2023.3.8")
-//    type.set("IC") // Target IDE Platform
-//
-//    plugins.set(listOf(/* Plugin Dependencies */))
-//}
-
-
 intellij {
-    updateSinceUntilBuild.set(false) // 关闭构建范围检查
-    localPath.set("C:\\Users\\Administrator\\.gradle\\caches\\modules-2\\files-2.1\\com.jetbrains.intellij.idea\\ideaIC\\2023.3.8\\bfbaf3e3e0fded07b36765dd8ac98c3901d3e106")
+/*    updateSinceUntilBuild.set(false) // 关闭构建范围检查
+//    localPath.set("C:\\Users\\Administrator\\.gradle\\caches\\modules-2\\files-2.1\\com.jetbrains.intellij.idea\\ideaIC\\2023.3.8\\bfbaf3e3e0fded07b36765dd8ac98c3901d3e106")
     plugins.set(listOf("java", "org.jetbrains.kotlin"))  // 加载依赖插件
     plugins = listOf("java")
+    version = "2021.1"
+    type = "IU"*/
 
+    version.set("2023.3.8") //支持的 IDEA 版本
+    type.set("IU") // 或 IC，与你使用的 IntelliJ 一致
+    updateSinceUntilBuild.set(false)
+    plugins.set(listOf("java", "org.jetbrains.kotlin"))
 }
 dependencies {
 //    implementation("io.github.java-diff-utils:java-diff-utils:4.12")
@@ -96,6 +92,11 @@ tasks {
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
+    }
+    // 添加这段，确保依赖被打进插件 jar
+    jar {
+        from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     }
 
     patchPluginXml {
